5637f53030d29a521f8403c591dcbce48a187114
mobettadanyu <mobettadanyu@gmail.com>
2023-11-02 23:20:13 -0500

    Recoded scraper to ensure RAM sustainability
    Previously, RAM shortage was an issue with exponentially growing amount of requests. To combat this, there is a static number of threads that reference a list of links to scrape.

---
+++ scraper/src/alex_not_cleaned.js
@@ -0,0 +1,121 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import pdf from 'pdf-parse-fork'
+import { convert } from 'html-to-text'
+import 'colors'
+import './logger.js'
+const WHITELIST_DOMAINS = [
+  'www.cod.edu',
+  'catalog.cod.edu'
+]
+const BLACKLIST_MATCHES = [
+  'https://www.cod.edu/about/police_department/pdf/incident_reports/',
+  'https://www.cod.edu/about/administration/planning_and_reporting_documents/pdf/disbursements',
+  'https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning',
+  'https://www.cod.edu/faculty/websites/pearson/documents/student-portfolios/',
+  'https://www.cod.edu/about/purchasing'
+  // 'dc.cod.edu',
+  // 'library.cod.edu'
+]
+
+const OUT_FOLDER = 'scraped/'
+
+var numberOfThreads=60;
+
+export const stats = {
+  linksCrawled: 0,
+  linksSaved: 0,
+}
+//link=[[url, amountofRetries]]
+var links=[['https://www.cod.edu',0],['https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning-as-21-23.pdf',0]]
+var index=0
+//exponentialBackoff is the amount of time per thread to wait before scraping again --> measure for slowing down scrape if website complains.
+//It expotentially backs off 
+async function scrape(exponentialBackoff) {
+  try {
+    try{
+    var url=links[index][0]
+    var amountOfRetries=links[index][1]
+    }catch{
+      throw new Error("shutting down thread")
+    }
+    index++
+    stats.linksCrawled++
+    //console.log(`Fetching ${url}...`)
+    const res = await fetch(url, { rejectUnauthorized: false })
+    const html = await res.text()
+    const $ = cheerio.load(html)
+
+    if (url.endsWith('.pdf')) {
+      const data = await pdf(res)
+      const urlToFilePath = OUT_FOLDER + new URL(url).hostname.replace(/\//g, '-') + new URL(url).pathname.replace(/\//g, '-') + '.txt'
+      fs.appendFile(urlToFilePath, url + "\n" + data.text, (err) => {})
+      stats.linksSaved++
+    }else{
+    //save html page in foler with link
+    const urlToFilePath = OUT_FOLDER + new URL(url).hostname.replace(/\//g, '-') + new URL(url).pathname.replace(/\//g, '-') + '.txt'
+    const result = await Promise.resolve(convert(html))
+    fs.appendFile(urlToFilePath, url + '\n' + result, (err) => {})
+    stats.linksSaved++
+    
+    $('a').each((_, element) => {
+      var link = $(element).attr('href')
+      if(!link){return}
+      if (link[0] == '/') {
+        link = 'https://www.cod.edu' + link //for relative URLs
+      }
+      if (link.startsWith('..')) {
+        link = url + link    //check if this is right, might need to cut off the ..
+      }
+        if (!link.endsWith('.zip')) {
+        try{
+        var newUrl = new URL(link)
+        }catch{
+          return //not a link
+        }
+        if (WHITELIST_DOMAINS.includes(newUrl.hostname)) {
+        // Loop through BLACKLIST_MATCHES to check for includes on each individual string, not just generally in BLACKLIST_MATCHES
+        for (let i = 0; i < BLACKLIST_MATCHES.length; i++) {
+          if (link.includes(BLACKLIST_MATCHES[i])) {
+            return
+            }
+          }
+          if(!links.includes(link)) {
+            links.push([link,amountOfRetries])
+        }
+      }
+      }
+    })
+  }
+    if(exponentialBackoff!=1){
+    scrape(exponentialBackoff/2)
+    }else{
+    scrape(exponentialBackoff) //no errors continue scraping immediately
+    }
+  }catch(err){
+    console.error(err)
+    if(err=="shutting down thread"){
+      return
+    }
+    if(exponentialBackoff>32){
+      console.log('Too many errors: you might be accessing COD website too fast. Shutting thread down.')
+    }else{
+      if(amountOfRetries<5){  
+      links.push([url,amountOfRetries+1])
+      console.log('too many retries, skipping link.')  
+      }
+        scrape(exponentialBackoff*2)
+      }
+    }
+}
+
+//boot 60 scraping threads --> in the future maybe, depending on ram check how many concurrent requests can be made and go from there
+var threads=setInterval(function(){
+console.log('starting thread...')
+scrape(1);
+numberOfThreads--
+if(numberOfThreads==0){
+clearInterval(threads)
+}
+},1000)
--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,104 +1,137 @@
-import fetch from 'node-fetch'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import pdf from 'pdf-parse-fork'
-import { convert } from 'html-to-text'
-import 'colors'
-
-import './logger.js'
-
-const baseUrl = 'https://www.cod.edu'
-
-const WHITELIST_DOMAINS = [
-  'www.cod.edu',
-  'catalog.cod.edu'
-]
-
+import fetch from "node-fetch";
+import fs from "fs";
+import cheerio from "cheerio";
+import pdf from "pdf-parse-fork";
+import { convert } from "html-to-text";
+import "colors";
+import "./logger.js";
+const WHITELIST_DOMAINS = ["www.cod.edu", "catalog.cod.edu"];
 const BLACKLIST_MATCHES = [
-  'https://www.cod.edu/about/police_department/pdf/incident_reports/',
-  'https://www.cod.edu/about/administration/planning_and_reporting_documents/pdf/disbursements',
-  'https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning',
-  'https://www.cod.edu/faculty/websites/pearson/documents/student-portfolios/',
-  'https://www.cod.edu/about/purchasing'
+  "https://www.cod.edu/about/police_department/pdf/incident_reports/",
+  "https://www.cod.edu/about/administration/planning_and_reporting_documents/pdf/disbursements",
+  "https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning",
+  "https://www.cod.edu/faculty/websites/pearson/documents/student-portfolios/",
+  "https://www.cod.edu/about/purchasing",
   // 'dc.cod.edu',
   // 'library.cod.edu'
-]
+];
 
-const OUT_FOLDER = 'scraped/'
+const OUT_FOLDER = "scraped/";
 
-const allLinks = new Set()
+var numberOfThreads = 60;
 
 export const stats = {
   linksCrawled: 0,
   linksSaved: 0,
-}
-
-scrape(baseUrl, 1)
-scrape('https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning-as-21-23.pdf', 1)
-
-async function scrape(url, retrySeconds) {
+};
+//link=[[url, amountofRetries]]
+var links = [
+  ["https://www.cod.edu", 0],
+  [
+    "https://www.cod.edu/student_life/resources/counseling/pdf/student_planning/student-planning-as-21-23.pdf",
+    0,
+  ],
+];
+var index = 0;
+//exponentialBackoff is the amount of time per thread to wait before scraping again --> measure for slowing down scrape if website complains.
+//It expotentially backs off
+async function scrape(exponentialBackoff) {
   try {
-    // console.log(`Fetching ${url}...`)
-    stats.linksCrawled++
-
-    const res = await fetch(url, { rejectUnauthorized: false })
-    const html = await res.text()
-    const $ = cheerio.load(html)
-
-    if (url.endsWith('.pdf')) {
-      const data = await pdf(res)
-      const urlToFilePath = OUT_FOLDER + new URL(url).hostname.replace(/\//g, '-') + new URL(url).pathname.replace(/\//g, '-') + '.txt'
-      fs.appendFile(urlToFilePath, url + "\n" + data.text, (err) => {})
-      return
+    try {
+      var url = links[index][0];
+      var amountOfRetries = links[index][1];
+    } catch {
+      throw new Error("shutting down thread");
     }
+    index++;
+    stats.linksCrawled++;
+    //console.log(`Fetching ${url}...`)
+    const res = await fetch(url, { rejectUnauthorized: false });
+    const html = await res.text();
+    const $ = cheerio.load(html);
 
-    //save html page in foler with link
-    const urlToFilePath = OUT_FOLDER + new URL(url).hostname.replace(/\//g, '-') + new URL(url).pathname.replace(/\//g, '-') + '.txt'
-    const result = await Promise.resolve(convert(html))
-    fs.appendFile(urlToFilePath, url + '\n' + result, (err) => {})
-    
-    $('a').each((_, element) => {
-      let link = $(element).attr('href')
-      if (link[0] == '/') {
-        link = baseUrl + link
-      }
-      if (link.startsWith('..')) {
-        link = url + link
-      }
-      try {
-        if (!link.startsWith('http') || link.endsWith('.zip')) {
-          return
+    if (url.endsWith(".pdf")) {
+      const data = await pdf(res);
+      const urlToFilePath =
+        OUT_FOLDER +
+        new URL(url).hostname.replace(/\//g, "-") +
+        new URL(url).pathname.replace(/\//g, "-") +
+        ".txt";
+      fs.appendFile(urlToFilePath, url + "\n" + data.text, (err) => {});
+      stats.linksSaved++;
+    } else {
+      //save html page in foler with link
+      const urlToFilePath =
+        OUT_FOLDER +
+        new URL(url).hostname.replace(/\//g, "-") +
+        new URL(url).pathname.replace(/\//g, "-") +
+        ".txt";
+      const result = await Promise.resolve(convert(html));
+      fs.appendFile(urlToFilePath, url + "\n" + result, (err) => {});
+      stats.linksSaved++;
+
+      $("a").each((_, element) => {
+        var link = $(element).attr("href");
+        if (!link) {
+          return;
         }
-        const newUrl = new URL(link)
-        if (!WHITELIST_DOMAINS.includes(newUrl.hostname)) {
-          return
+        if (link[0] == "/") {
+          link = "https://www.cod.edu" + link; //for relative URLs
         }
-        // Loop through BLACKLIST_MATCHES with a for loop and check if link is included in a blacklist match
-        for (let i = 0; i < BLACKLIST_MATCHES.length; i++) {
-          if (link.includes(BLACKLIST_MATCHES[i])) {
-            return
+        if (link.startsWith("..")) {
+          link = url + link; //check if this is right, might need to cut off the ..
+        }
+        if (!link.endsWith(".zip")) {
+          try {
+            var newUrl = new URL(link);
+            if (WHITELIST_DOMAINS.includes(newUrl.hostname)) {
+              // Loop through BLACKLIST_MATCHES to check for includes on each individual string, not just generally in BLACKLIST_MATCHES
+              for (let i = 0; i < BLACKLIST_MATCHES.length; i++) {
+                if (link.includes(BLACKLIST_MATCHES[i])) {
+                  return;
+                }
+              }
+              if (!links.includes(link)) {
+                links.push([link, amountOfRetries]);
+                fs.appendFile('links.txt', link + '\n', (err) => {})
+              }
+            }
+          } catch {
+            return; //not a link
           }
         }
-        if (!allLinks.has(link)) {
-          allLinks.add(link)
-          fs.appendFile('links.txt', link + '\n', (err) => {})
-          stats.linksSaved++
-          scrape(link, 1)
-        }
+      });
+    }
+    if (exponentialBackoff != 1) {
+      scrape(exponentialBackoff / 2);
+    } else {
+      scrape(exponentialBackoff); //no errors continue scraping immediately
+    }
+  } catch (err) {
+    if(err.code!="UNABLE_TO_VERIFY_LEAF_SIGNATURE"){
+    console.error(err);
+    }
+    if (err == "shutting down thread") {
+      return;
+    }
+    if (exponentialBackoff > 32) {
+      console.log("Too many errors: you might be accessing COD website too fast. Shutting thread down.",);
+    } else {
+      if (amountOfRetries < 5) {
+        links.push([url, amountOfRetries + 1]);
+        console.log("too many retries, skipping link.");
       }
-      catch (error) {
-        console.log('Not a link: ' + link)
-      }
-    })
-  }
-  catch (err) {
-    // console.error(err) // errors confusing, so commented out
-    setTimeout(() => {
-      if (retrySeconds < 32) {
-        scrape(url, retrySeconds * 2)
-        return
-      }
-      console.log('too many retries, skipping link.')
-    }, retrySeconds * 1000)
+      scrape(exponentialBackoff * 2);
+    }
   }
 }
+
+//boot 60 scraping threads --> in the future maybe, depending on ram check how many concurrent requests can be made and go from there
+var threads = setInterval(function () {
+  console.log("starting thread...");
+  scrape(1);
+  numberOfThreads--;
+  if (numberOfThreads == 0) {
+    clearInterval(threads);
+  }
+}, 1000);
